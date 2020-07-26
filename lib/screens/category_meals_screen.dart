34fae634a8c62d4a6a50eb46cbc08f71c8b3a08f
harshthakur066 <harshthakur066@gmail.com>
2020-07-27 00:58:57 +0530

    added images and data passed in menu item page

--- lib/screens/category_meals_screen.dart
+++ lib/screens/category_meals_screen.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:recipeze/widgets/meal_item.dart';
 
 import '../models/dummy_data.dart';
 
@@ -23,7 +24,13 @@ class CategoryMealsScreen extends StatelessWidget {
         ),
         body: ListView.builder(
           itemBuilder: (ctx, index) {
-            return Text(categoryMeals[index].title);
+            return MealItem(
+              title: categoryMeals[index].title,
+              imageUrl: categoryMeals[index].imageUrl,
+              duration: categoryMeals[index].duration,
+              complexity: categoryMeals[index].complexity,
+              affordability: categoryMeals[index].affordability,
+            );
           },
           itemCount: categoryMeals.length,
         ));
