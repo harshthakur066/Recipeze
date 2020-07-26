55051229110bd8aa47917bf660165f01b1377128
harshthakur066 <harshthakur066@gmail.com>
2020-07-26 23:51:28 +0530

    added meal data and passing the data

---
+++ lib/screens/meal_detail_screen.dart
@@ -0,0 +1,10 @@
+import 'package:flutter/widgets.dart';
+
+class MealDetailScreen extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Center(
+      child: Text('Meal Detail'),
+    );
+  }
+}
